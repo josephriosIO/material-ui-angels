351aa3d3a8a9c09b60a614d7398a8d7272769ca6
josephriosio <blankworldwide@gmail.com>
2019-12-02 17:57:13 -0800

    only when logged in can you click on admin page

---
+++ material-ui-with-login/src/components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,18 @@
+import '@reshuffle/code-transform/macro';
+import React from 'react';
+import { useAuth } from '@reshuffle/react-auth';
+import { Route, Redirect } from 'react-router-dom';
+
+const Protected = ({ component: Component, ...rest }) => {
+  const { authenticated } = useAuth();
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authenticated ? <Component {...props} /> : <Redirect to='/' />
+      }
+    />
+  );
+};
+
+export default Protected;
