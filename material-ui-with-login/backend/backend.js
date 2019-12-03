d88c4feb15e8166773dc3d54ddcd7bf4a3a1e06b
josephriosio <blankworldwide@gmail.com>
2019-12-02 17:31:24 -0800

    added get users and add users to backend

--- material-ui-with-login/backend/backend.js
+++ material-ui-with-login/backend/backend.js
@@ -1,4 +1,38 @@
+import { get, update } from '@reshuffle/db';
+import { getCurrentUser } from '@reshuffle/server-function';
+
 /* @expose */
-export async function hello(name) {
-  return `Hello ${name}!`;
+export async function testing(name, pic) {
+  const { id } = getCurrentUser(true);
+
+  return update('testagainagainagain', (users = []) => {
+    let allUsers = JSON.parse(JSON.stringify(users));
+
+    const user = {
+      id,
+      name,
+      img: pic,
+      admin: true,
+      angel: false,
+      user: true,
+    };
+    if (allUsers.length > 0) {
+      user.admin = false;
+    }
+
+    allUsers.push(user);
+
+    allUsers = allUsers.filter(
+      (user, index, self) => index === self.findIndex(t => t.id === user.id),
+    );
+
+    return allUsers;
+  });
+}
+
+/* @expose */
+export async function getUsers() {
+  const users = await get('testagainagainagain');
+
+  return users;
 }
