b0a20e78be2fa98130671e3fde6439a8d504357d
josephriosio <blankworldwide@gmail.com>
2019-12-02 17:56:34 -0800

    added privateRoutes and admin page

--- material-ui-with-login/src/App.js
+++ material-ui-with-login/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import Protected from './components/PrivateRoute/PrivateRoute';
+import AdminPage from './components/AdminPage';
 
 const App = () => {
-  return <Route exact path='/' component={Dashboard} />;
+  return (
+    <>
+      <Route exact path='/' component={Dashboard} />
+      <Protected exact path='/admin' component={AdminPage} />
+    </>
+  );
 };
 export default App;
