15502cad97a42d8b4fe66d9a3b85d29f897d435b
josephriosio <blankworldwide@gmail.com>
2019-12-02 17:45:31 -0800

    added Router

--- material-ui-with-login/src/index.js
+++ material-ui-with-login/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from '@reshuffle/react-auth';
 
 import App from './App';
 import './index.css';
 
-
-ReactDOM.render((
-  <AuthProvider>
-    <App />
-  </AuthProvider>
-), document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </Router>,
+  document.getElementById('root'),
+);
