cd269ad7968e6d38bca36d9347e14d874ee6af03
josephriosio <blankworldwide@gmail.com>
2019-12-02 18:05:34 -0800

    added navbar to app

--- material-ui-with-login/src/App.js
+++ material-ui-with-login/src/App.js
@@ -3,10 +3,12 @@ import { Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Protected from './components/PrivateRoute/PrivateRoute';
 import AdminPage from './components/AdminPage';
+import NavBar from './components/Nav/NavBar';
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Route exact path='/' component={Dashboard} />
       <Protected exact path='/admin' component={AdminPage} />
     </>
