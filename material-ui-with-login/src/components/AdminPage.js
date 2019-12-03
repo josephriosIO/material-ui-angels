9738e331812ec09df8e92e131e680179d422a2c6
josephriosio <blankworldwide@gmail.com>
2019-12-03 11:15:43 -0800

    moved data to new component to display

--- material-ui-with-login/src/components/AdminPage.js
+++ material-ui-with-login/src/components/AdminPage.js
@@ -3,6 +3,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
+import DisplayUsers from './DisplayUsers';
 
 const useStyles = makeStyles(theme => ({
   heroContent: {
@@ -13,20 +14,24 @@ const useStyles = makeStyles(theme => ({
 const AdminPage = props => {
   const classes = useStyles();
   const { users } = props.location.state;
-  console.log(users);
+
   return (
     <>
       <CssBaseline />
       <Container maxWidth='sm' component='main' className={classes.heroContent}>
-        <Typography
-          component='h1'
-          variant='h2'
-          align='center'
-          color='textSecondary'
-          gutterBottom
-        >
-          Users
-        </Typography>
+        {users.length > 0 ? (
+          users.map(user => <DisplayUsers user={user} />)
+        ) : (
+          <Typography
+            component='h1'
+            variant='h2'
+            align='center'
+            color='textSecondary'
+            gutterBottom
+          >
+            No Users
+          </Typography>
+        )}
       </Container>
     </>
   );
