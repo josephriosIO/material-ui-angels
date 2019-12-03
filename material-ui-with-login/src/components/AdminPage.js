192242b602a3c422fa6d761dfbc96ff82aecdaf7
josephriosio <blankworldwide@gmail.com>
2019-12-03 10:54:01 -0800

    added basic styling to admin page

--- material-ui-with-login/src/components/AdminPage.js
+++ material-ui-with-login/src/components/AdminPage.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
 
-const AdminPage = () => {
+const useStyles = makeStyles(theme => ({
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+}));
+
+const AdminPage = props => {
+  const classes = useStyles();
+  const { users } = props.location.state;
+  console.log(users);
   return (
-    <div>
-      <p>Testing</p>
-    </div>
+    <>
+      <CssBaseline />
+      <Container maxWidth='sm' component='main' className={classes.heroContent}>
+        <Typography
+          component='h1'
+          variant='h2'
+          align='center'
+          color='textSecondary'
+          gutterBottom
+        >
+          Users
+        </Typography>
+      </Container>
+    </>
   );
 };
 
