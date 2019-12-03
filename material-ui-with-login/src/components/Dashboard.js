47b5b909bdfe8f8f0f3762bf59552f6894da87a4
josephriosio <blankworldwide@gmail.com>
2019-12-02 18:13:04 -0800

    removed navbar files

--- material-ui-with-login/src/components/Dashboard.js
+++ material-ui-with-login/src/components/Dashboard.js
@@ -3,13 +3,8 @@ import React, { useState, useEffect } from 'react';
 import { useAuth } from '@reshuffle/react-auth';
 import { testing, getUsers } from '../../backend/backend';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-import { NavLink } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles(theme => ({
@@ -24,18 +19,6 @@ const useStyles = makeStyles(theme => ({
     li: {
       listStyle: 'none',
     },
-  },
-  appBar: {
-    borderBottom: `1px solid ${theme.palette.divider}`,
-  },
-  toolbar: {
-    flexWrap: 'wrap',
-  },
-  toolbarTitle: {
-    flexGrow: 1,
-  },
-  link: {
-    margin: theme.spacing(1, 1.5),
   },
   heroContent: {
     padding: theme.spacing(8, 0, 6),
