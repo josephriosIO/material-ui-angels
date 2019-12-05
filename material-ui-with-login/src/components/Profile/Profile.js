43b3533c2e8f9de8e4b909468d986ab723fc2bbc
josephriosio <blankworldwide@gmail.com>
2019-12-05 10:30:23 -0800

    finally got form to submit data

--- material-ui-with-login/src/components/Profile/Profile.js
+++ material-ui-with-login/src/components/Profile/Profile.js
@@ -1,20 +1,10 @@
 import '@reshuffle/code-transform/macro';
 import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { getUser, updateProfile } from '../../../backend/backend';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      width: 200,
-    },
-  },
-}));
 
 const Profile = props => {
   const [profile, setProfile] = useState({});
@@ -24,7 +14,6 @@ const Profile = props => {
     bio: '',
     phoneNumber: '',
   });
-  const classes = useStyles();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -36,70 +25,73 @@ const Profile = props => {
   }, []);
 
   const handleSubmits = async event => {
+    console.log('running');
     event.preventDefault();
-    await updateProfile(profile);
+    const saved = await updateProfile(form);
+    console.log(saved);
   };
 
   const onChange = e => setForm({ ...form, [e.target.name]: e.target.value });
 
+  console.log(form);
   return (
     <React.Fragment>
       <Typography variant='h6' gutterBottom>
         Update Profile
       </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <TextField
-            onChange={e => onChange(e)}
-            defaultValue={profile.name}
-            placeholder='Placeholder'
-            id='standard-normal'
-            name='name'
-            label='Name'
-            fullWidth
-          />
-        </Grid>
+      <form onSubmit={handleSubmits}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <TextField
+              onChange={e => onChange(e)}
+              defaultValue={profile.name}
+              placeholder='Placeholder'
+              id='standard-normal'
+              name='name'
+              label='Name'
+              fullWidth
+            />
+          </Grid>
 
-        <Grid item xs={12}>
-          <TextField
-            onChange={e => onChange(e)}
-            defaultValue={profile.location}
-            id='location'
-            name='location'
-            label='Location'
-            fullWidth
-          />
-        </Grid>
+          <Grid item xs={12}>
+            <TextField
+              onChange={e => onChange(e)}
+              defaultValue={profile.location}
+              id='location'
+              name='location'
+              label='Location'
+              fullWidth
+            />
+          </Grid>
 
-        <Grid item xs={12} sm={6}>
-          <TextField
-            defaultValue={profile.phoneNumber}
-            onChange={e => onChange(e)}
-            id='number'
-            name='phoneNumber'
-            label='Phone Number'
-            fullWidth
-            autoComplete='phone-number'
-          />
+          <Grid item xs={12} sm={6}>
+            <TextField
+              defaultValue={profile.phoneNumber}
+              onChange={e => onChange(e)}
+              id='number'
+              name='phoneNumber'
+              label='Phone Number'
+              fullWidth
+              autoComplete='phone-number'
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              defaultValue={profile.bio}
+              onChange={e => onChange(e)}
+              id='bio'
+              name='bio'
+              label='Bio'
+              fullWidth
+              multiline
+              rows='4'
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            defaultValue={profile.bio}
-            onChange={e => onChange(e)}
-            id='bio'
-            name='bio'
-            label='Bio'
-            fullWidth
-          />
-        </Grid>
-        <Button
-          onSubmit={e => handleSubmits(e)}
-          variant='contained'
-          color='primary'
-        >
+        <Button type='submit' variant='contained' color='primary'>
           Submit
         </Button>
-      </Grid>
+      </form>
     </React.Fragment>
   );
 };
