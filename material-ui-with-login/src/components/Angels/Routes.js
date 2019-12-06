0c1efdc06dd8f2ea13babb4a6544c9b40fafa751
josephriosio <blankworldwide@gmail.com>
2019-12-06 14:30:50 -0800

    created route holder for angels

---
+++ material-ui-with-login/src/components/Angels/Routes.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+import Dashboard from './Dashboard';
+import AdminPage from './AdminPage';
+import Profile from './Profile/Profile';
+import NavBar from './Nav/NavBar';
+
+import Protected from '../PrivateRoute/PrivateRoute';
+
+const Routes = props => {
+  return (
+    <>
+      <NavBar />
+      <Route exact path={props.match.path} component={Dashboard} />
+      <Protected
+        exact
+        path={`${props.match.path}/admin`}
+        component={AdminPage}
+      />
+      <Protected
+        exact
+        path={`${props.match.path}/profile/:id`}
+        component={Profile}
+      />
+    </>
+  );
+};
+
+export default Routes;
