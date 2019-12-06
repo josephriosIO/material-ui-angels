4075a43acf5254e2169e1215d0c560e74d6f208e
josephriosio <blankworldwide@gmail.com>
2019-12-06 11:02:18 -0800

    hide navbar on landing page

--- material-ui-with-login/src/App.js
+++ material-ui-with-login/src/App.js
@@ -5,12 +5,16 @@ import Protected from './components/PrivateRoute/PrivateRoute';
 import AdminPage from './components/AdminPage';
 import NavBar from './components/Nav/NavBar';
 import Profile from './components/Profile/Profile';
+import LandingPage from './components/LandingPage/LandingPage';
 
-const App = () => {
+const App = props => {
+  const { pathname } = window.location;
+
   return (
     <>
-      <NavBar />
-      <Route exact path='/' component={Dashboard} />
+      {pathname !== '/' ? <NavBar /> : null}
+      <Route exact path='/' component={LandingPage} />
+      <Route exact path='/angels' component={Dashboard} />
       <Protected exact path='/admin' component={AdminPage} />
       <Protected exact path='/profile/:id' component={Profile} />
     </>
