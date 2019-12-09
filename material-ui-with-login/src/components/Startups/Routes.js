479f65ad3c0e7d93347bad216da4be6f786ec95e
josephriosio <blankworldwide@gmail.com>
2019-12-09 15:00:24 -0800

    added protected routes

--- material-ui-with-login/src/components/Startups/Routes.js
+++ material-ui-with-login/src/components/Startups/Routes.js
@@ -4,17 +4,18 @@ import Dashboard from './Dashboard';
 import NavBar from './Nav/Navbar';
 import Questionaire from './Questionaire/Questionaire';
 import Profile from './Profile/Profile';
+import Protected from '../PrivateRoute/PrivateRoute';
 
 const Routes = props => {
   return (
     <>
       <NavBar />
       <Route exact path={props.match.path} component={Dashboard} />
-      <Route
+      <Protected
         path={`${props.match.path}/questionaire`}
         component={Questionaire}
       />
-      <Route path={`${props.match.path}/profile/:id`} component={Profile} />
+      <Protected path={`${props.match.path}/profile/:id`} component={Profile} />
     </>
   );
 };
