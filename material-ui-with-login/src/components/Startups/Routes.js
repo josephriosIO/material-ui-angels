beb2a8a30b99f34aa090b3d3833fba4e4555bf80
josephriosio <blankworldwide@gmail.com>
2019-12-09 12:12:08 -0800

    added navbar to routes for startups

--- material-ui-with-login/src/components/Startups/Routes.js
+++ material-ui-with-login/src/components/Startups/Routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
+import NavBar from './Nav/Navbar';
 
 const Routes = props => {
   return (
     <>
+      <NavBar />
       <Route exact path={props.match.path} component={Dashboard} />
     </>
   );
