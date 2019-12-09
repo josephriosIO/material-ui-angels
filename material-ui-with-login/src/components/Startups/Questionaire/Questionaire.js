e9f54dc088fa30dad3ac54534f72a7e2e22aaafb
josephriosio <blankworldwide@gmail.com>
2019-12-09 12:25:56 -0800

    filled out basic questions for questionaire

--- material-ui-with-login/src/components/Startups/Questionaire/Questionaire.js
+++ material-ui-with-login/src/components/Startups/Questionaire/Questionaire.js
@@ -3,7 +3,30 @@ import React from 'react';
 const Questionaire = () => {
   return (
     <div>
-      <p>Questionaire goes</p>
+      <h2>Fill out some info about your startup below!</h2>
+      <form>
+        <div>
+          <label>What is your companys name?</label>
+          <input />
+        </div>
+        <div>
+          <label>What is your company located?</label>
+          <input />
+        </div>
+        <div>
+          <label>How many people does your company employee?</label>
+          <input />
+        </div>
+        <div>
+          <label>Is the company funded?</label>
+          <input />
+        </div>
+        <div>
+          <label>What is your companys mission statement?</label>
+          <input />
+        </div>
+        <button>submit</button>
+      </form>
     </div>
   );
 };
