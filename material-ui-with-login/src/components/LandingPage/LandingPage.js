b196a32ae9fc42e8403a5e11b91d7a613003157a
josephriosio <blankworldwide@gmail.com>
2019-12-09 13:28:37 -0800

    took off gret coloring

--- material-ui-with-login/src/components/LandingPage/LandingPage.js
+++ material-ui-with-login/src/components/LandingPage/LandingPage.js
@@ -153,9 +153,7 @@ const LandingPage = () => {
       >
         <div className={`${matches ? classes.boxes : classes.boxesMobile}`}>
           <p>Are you a Start-up?</p>
-          <ColorButton style={{ background: 'grey' }} href='/startups'>
-            Login
-          </ColorButton>
+          <ColorButton href='/startups'>Login</ColorButton>
         </div>
 
         <div className={`${matches ? classes.boxes : classes.boxesMobile}`}>
