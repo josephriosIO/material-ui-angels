8ff5306907041964161e3ec20530b886a140b761
josephriosio <blankworldwide@gmail.com>
2019-12-09 12:25:39 -0800

    added routes for startups

--- material-ui-with-login/src/components/Startups/Routes.js
+++ material-ui-with-login/src/components/Startups/Routes.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import NavBar from './Nav/Navbar';
+import Questionaire from './Questionaire/Questionaire';
+import Profile from './Profile/Profile';
 
 const Routes = props => {
   return (
     <>
       <NavBar />
       <Route exact path={props.match.path} component={Dashboard} />
+      <Route
+        path={`${props.match.path}/questionaire`}
+        component={Questionaire}
+      />
+      <Route path={`${props.match.path}/profile/:id`} component={Profile} />
     </>
   );
 };
