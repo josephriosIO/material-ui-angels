3da1921059af4d1382708f76d4de4ef8837c4d86
josephriosio <blankworldwide@gmail.com>
2019-12-10 14:43:29 -0800

    remade theme

--- material-ui-with-login/src/components/LandingPage/LandingPage.js
+++ material-ui-with-login/src/components/LandingPage/LandingPage.js
@@ -4,149 +4,158 @@ import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { Helmet } from 'react-helmet';
+import GroupIcon from '@material-ui/icons/Group';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import AllInclusiveIcon from '@material-ui/icons/AllInclusive';
 
 const useStyles = makeStyles({
-  boxContainer: {
-    width: '100%',
-    display: 'flex',
-    flexFlow: 'row',
-    background: 'linear-gradient(160deg, #FE6B8B 30%, #FF8E53 80%)',
-    flexWrap: 'wrap',
-    justifyContent: 'space-evenly',
-  },
-  boxContainerMobile: {
-    width: '100%',
-    display: 'flex',
-    flexFlow: 'column',
-    background: 'linear-gradient(155deg, #FE6B8B 20%, #FF8E53 65%)',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   title: {
+    color: 'black',
     fontSize: '38px',
     lineHeight: 1.05,
     fontWeight: 800,
     textAlign: 'center',
   },
+  logo: {
+    color: 'black',
+    display: 'flex',
+    alignItems: 'baseline',
+  },
   header: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
     width: '100%',
     height: '300px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
     padding: '10px 30px',
     display: 'flex',
     flexFlow: 'column',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  footer: {
-    background: 'linear-gradient(40deg, #FE6B8B 20%, #FF8E53 90%)',
-    width: '100%',
-    height: '300px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    padding: '10px 30px',
+  body: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  bodyMobile: {
     display: 'flex',
     flexFlow: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mobileBodyItems: {
+    display: 'flex',
+    flexFlow: 'column',
+    width: '85%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: '30px',
+  },
+  bodyHeaders: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  bodyHeadersMobile: {
+    flexFlow: 'column-reverse',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    height: '2.5rem' /* Footer height */,
+    display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
   },
   footerMobile: {
-    background: 'linear-gradient(360deg, #FE6B8B 30%, #FF8E53 90%)',
+    bottom: 0,
     width: '100%',
-    height: '300px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    padding: '10px 30px',
+    height: '3.5rem' /* Footer height */,
     display: 'flex',
-    flexFlow: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  boxes: {
-    display: 'flex',
-    width: '50%',
-    flexFlow: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'white',
-    paddingBottom: '20px',
-    marginBottom: '40px',
-
-    borderRadius: '29px',
-  },
-  boxesMobile: {
-    display: 'flex',
-    width: '90%',
-    flexFlow: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'white',
-    paddingBottom: '20px',
-    marginBottom: '40px',
-
-    borderRadius: '29px',
   },
 });
 
 const ColorButton = withStyles(theme => ({
   root: {
-    color: 'white !important',
-    width: '120px',
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    color: '#494949 !important',
+    textTransform: 'uppercase',
+    textDecoration: 'none',
+    background: '#fff',
+    padding: '20px',
+    border: '4px solid #494949 !important',
+    display: 'inline-block',
+    transition: 'all 0.4s ease 0s',
     '&:hover': {
-      background: 'linear-gradient(180deg, #FE6B8B 10%, #FF8E53 50%)',
+      color: '#ffffff !important',
+      background: '#f6b93b',
+      borderColor: '#f6b93b !important',
+      transition: 'all 0.4s ease 0s',
     },
   },
 }))(Button);
 
 const LandingPage = () => {
   const classes = useStyles();
-  const matches = useMediaQuery('(min-width:600px)');
+  const matches = useMediaQuery('(min-width:800px)');
 
   return (
     <>
-      <Helmet>
-        <style>
-          {`
-            html { 
-                height: 100%;
-            } 
-            body { 
-                height: 100vh;
-                margin: 0;
-                background-repeat: no-repeat;
-                background-attachment: fixed; 
-                background: #FE6B8B;
-                 }`}
-        </style>
-      </Helmet>
       <div className={classes.header}>
-        <p>Angels Group</p>
-        <h1 className={classes.title}>Where Start-ups meet angels</h1>
-      </div>
-      <div
-        className={`${
-          matches ? classes.boxContainer : classes.boxContainerMobile
-        }`}
-      >
-        <div className={`${matches ? classes.boxes : classes.boxesMobile}`}>
-          <p>Are you a Start-up?</p>
-          <ColorButton href='/startups'>Login</ColorButton>
+        <div className={classes.logo}>
+          <p>MeetAngels</p>
+          {'  '}
+          <span>
+            <AllInclusiveIcon />
+          </span>
         </div>
 
-        <div className={`${matches ? classes.boxes : classes.boxesMobile}`}>
-          <p>Are you an angel investor?</p>
-          <ColorButton href='/angels'>Login</ColorButton>
+        <h1 className={classes.title}>Where Start-ups meet angels</h1>
+      </div>
+      <div className={`${matches ? classes.body : classes.bodyMobile}`}>
+        <div className={`${matches ? null : classes.mobileBodyItems}`}>
+          <div
+            className={`${
+              matches ? classes.bodyHeaders : classes.bodyHeadersMobile
+            }`}
+          >
+            <h2>MeetStartups</h2>
+            <span>
+              <GroupIcon />
+            </span>
+          </div>
+          <p>
+            Login and set up your startups profile to start meeting angels
+            today!
+          </p>
+          <ColorButton href='/startups'>Login As Startup</ColorButton>
+        </div>
+
+        <div className={`${matches ? null : classes.mobileBodyItems}`}>
+          <div
+            className={`${
+              matches ? classes.bodyHeaders : classes.bodyHeadersMobile
+            }`}
+          >
+            <h2>MeetAngels</h2>
+            <span>
+              <TrendingUpIcon />
+            </span>
+          </div>
+
+          <p>
+            By one of the next angel investors that fund the next big company.
+          </p>
+          <ColorButton href='/angels'>Become An Angel</ColorButton>
         </div>
       </div>
-      <div className={`${matches ? classes.footer : classes.footerMobile}`}>
+      <footer className={`${matches ? classes.footer : classes.footerMobile}`}>
         <p>
           Powered by <a href='https://reshuffle.com'>Reshuffle</a>
         </p>
-      </div>
+      </footer>
     </>
   );
 };
