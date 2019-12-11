522511b8f078bca27b4e000fa605e67a8060cbee
josephriosio <blankworldwide@gmail.com>
2019-12-10 16:45:32 -0800

    updated dashboard

--- material-ui-with-login/src/components/Startups/Dashboard.js
+++ material-ui-with-login/src/components/Startups/Dashboard.js
@@ -1,29 +1,15 @@
 import '@reshuffle/code-transform/macro';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useAuth } from '@reshuffle/react-auth';
 import { Redirect } from 'react-router-dom';
-import { getStartup } from '../../../backend/backend';
+
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 const Dashboard = () => {
   const { authenticated } = useAuth();
-  const [profile, setProfile] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const user = await getStartup();
-
-      setProfile(...user);
-    };
-    fetchData();
-  }, []);
-
-  if (profile) {
-    if (authenticated && !profile.completed) {
-      return <Redirect to='/startups/questionaire' />;
-    } else if (authenticated && profile.completed) {
-      return <Redirect to='/startups/dashboard' />;
-    }
+  if (authenticated) {
+    return <Redirect to='/startups/questionaire' />;
   }
 
   return (
