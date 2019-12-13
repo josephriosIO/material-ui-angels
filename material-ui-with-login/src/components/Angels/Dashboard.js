691c801aac0727661db5374af5ba3387baa95084
josephriosio <blankworldwide@gmail.com>
2019-12-13 09:54:11 -0800

    updated dashboard for angels to be more readable

--- material-ui-with-login/src/components/Angels/Dashboard.js
+++ material-ui-with-login/src/components/Angels/Dashboard.js
@@ -1,46 +1,19 @@
 import '@reshuffle/code-transform/macro';
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '@reshuffle/react-auth';
-import { setUsersToBackend, getUsers } from '../../../backend/backend';
+import {
+  setUsersToBackend,
+  getUsers,
+  getAllAngels,
+  createOrGetUser,
+  getRole,
+} from '../../../backend/backend';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import Avatar from '@material-ui/core/Avatar';
-
-const columns = [
-  { id: 'name', align: 'center', label: 'Name', minWidth: 170 },
-  { id: 'location', align: 'right', label: 'Location', minWidth: 100 },
-  {
-    id: 'email',
-    label: 'Email',
-    minWidth: 170,
-    align: 'center',
-    format: value => value.toLocaleString(),
-  },
-  {
-    id: 'phoneNumber',
-    label: 'Phone Number',
-    minWidth: 170,
-    align: 'right',
-    format: value => value.toLocaleString(),
-  },
-  {
-    id: 'bio',
-    label: 'Bio',
-    minWidth: 170,
-    align: 'right',
-    format: value => value.toLocaleString(),
-  },
-];
+import EmptyState from './EmptyState';
+import DashboardTable from './DashboardTable';
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -77,17 +50,17 @@ const useStyles = makeStyles(theme => ({
 
 export default function Page() {
   const [users, setUsers] = useState([]);
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
   const classes = useStyles();
-  const { loading, profile, authenticated } = useAuth();
+  const { loading } = useAuth();
+  const [roles, setRoles] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await getUsers();
-      if (profile) {
-        await setUsersToBackend(profile.displayName, profile.picture);
-      }
+      const result = await getAllAngels();
+      const roles = await getRole();
+
+      setRoles(roles);
+
       if (result) {
         setUsers(result);
       }
@@ -95,15 +68,6 @@ export default function Page() {
     fetchData();
     // eslint-disable-next-line
   }, []);
-
-  const handleChangePage = (e, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = event => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
 
   // wait for the user data to load.
   if (loading) {
@@ -114,227 +78,49 @@ export default function Page() {
     );
   }
 
+  if (roles.ADMIN) {
+    return (
+      <>
+        <CssBaseline />
+        <Container
+          maxWidth='lg'
+          component='main'
+          className={classes.heroContent}
+        >
+          {users.length > 0 ? (
+            <DashboardTable users={users} />
+          ) : (
+            <EmptyState
+              title={'Admin View'}
+              subtitle={'Accept some angels.'}
+              roles={roles}
+              users={users}
+            />
+          )}
+        </Container>
+      </>
+    );
+  }
+
+  if (!roles.ANGEL && !roles.ADMIN) {
+    return (
+      <EmptyState
+        title={'Thank you for your request!'}
+        subtitle={
+          'An Admin will accept you shortly if you meet the requirements.'
+        }
+        roles={roles}
+        users={users}
+      />
+    );
+  }
+
   return (
     <React.Fragment>
       <CssBaseline />
-
       <Container maxWidth='lg' component='main' className={classes.heroContent}>
-        {authenticated ? (
-          users.filter(user => {
-            if (user.id === profile.id) {
-              if (user.admin === true) {
-                return user;
-              }
-            }
-          }).length > 0 ? (
-            users.filter(user => user.angel === true).length > 0 ? (
-              <Grid>
-                <Paper className={classes.root}>
-                  <h2>Angels</h2>
-                  <div className={classes.tableWrapper}>
-                    <Table stickyHeader aria-label='sticky table'>
-                      <TableHead>
-                        <TableRow>
-                          {columns.map(column => (
-                            <TableCell
-                              key={column.id}
-                              align={column.align}
-                              style={{ minWidth: column.minWidth }}
-                            >
-                              {column.label}
-                            </TableCell>
-                          ))}
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>
-                        {users
-                          .slice(
-                            page * rowsPerPage,
-                            page * rowsPerPage + rowsPerPage,
-                          )
-                          .map(row =>
-                            row.angel ? (
-                              <TableRow
-                                hover
-                                role='checkbox'
-                                tabIndex={-1}
-                                key={row.id}
-                              >
-                                {columns.map((column, count = 0) => {
-                                  count += 1;
-                                  let value = row[column.id];
-
-                                  if (Array.isArray(value)) {
-                                    value = value[0].value;
-                                  }
-
-                                  if (value === '' || value === null) {
-                                    value = 'N/A';
-                                  }
-
-                                  return (
-                                    <TableCell
-                                      key={column.id}
-                                      align={column.align}
-                                    >
-                                      <div
-                                        style={{
-                                          display: 'flex',
-                                          alignItems: 'center',
-                                          justifyContent: 'center',
-                                        }}
-                                      >
-                                        {count < 2 ? (
-                                          <Avatar
-                                            src={row.img}
-                                            alt={row.name}
-                                          />
-                                        ) : null}
-                                        {value}
-                                      </div>
-                                    </TableCell>
-                                  );
-                                })}
-                              </TableRow>
-                            ) : null,
-                          )}
-                      </TableBody>
-                    </Table>
-                  </div>
-                  <TablePagination
-                    rowsPerPageOptions={[10, 25, 100]}
-                    component='div'
-                    count={users.filter(user => user.angel === true).length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onChangePage={handleChangePage}
-                    onChangeRowsPerPage={handleChangeRowsPerPage}
-                  />
-                </Paper>
-              </Grid>
-            ) : (
-              <div class='empty'>
-                <div class='empty-icon'>
-                  <i class='icon icon-people'></i>
-                </div>
-                <p class='empty-title h5'>Admin View</p>
-                <p class='empty-subtitle'>Accept Some angels.</p>
-                <Link
-                  style={{ textDecoration: 'none', color: 'black' }}
-                  className={classes.link}
-                  to={{
-                    pathname: `/angels/admin`,
-                    state: {
-                      users: users,
-                    },
-                  }}
-                >
-                  Admin
-                </Link>
-              </div>
-            )
-          ) : users.filter(user => {
-              if (user.id === profile.id) {
-                if (user.angel === false) {
-                  return user;
-                }
-              }
-            }).length > 0 ? (
-            <div class='empty'>
-              <div class='empty-icon'>
-                <i class='icon icon-people'></i>
-              </div>
-              <p class='empty-title h5'>Thank you for your request!</p>
-              <p class='empty-subtitle'>
-                An Admin will accept you shortly if you meet the requirements.
-              </p>
-            </div>
-          ) : (
-            <Grid>
-              <Paper className={classes.root}>
-                <h2>Angels</h2>
-                <div className={classes.tableWrapper}>
-                  <Table stickyHeader aria-label='sticky table'>
-                    <TableHead>
-                      <TableRow>
-                        {columns.map(column => (
-                          <TableCell
-                            key={column.id}
-                            align={column.align}
-                            style={{ minWidth: column.minWidth }}
-                          >
-                            {column.label}
-                          </TableCell>
-                        ))}
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {users
-                        .slice(
-                          page * rowsPerPage,
-                          page * rowsPerPage + rowsPerPage,
-                        )
-                        .map(row =>
-                          row.angel ? (
-                            <TableRow
-                              hover
-                              role='checkbox'
-                              tabIndex={-1}
-                              key={row.id}
-                            >
-                              {columns.map((column, count = 0) => {
-                                count += 1;
-                                let value = row[column.id];
-
-                                if (Array.isArray(value)) {
-                                  value = value[0].value;
-                                }
-
-                                if (value === '' || value === null) {
-                                  value = 'N/A';
-                                }
-
-                                return (
-                                  <TableCell
-                                    key={column.id}
-                                    align={column.align}
-                                  >
-                                    <div
-                                      style={{
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        justifyContent: 'center',
-                                      }}
-                                    >
-                                      {count < 2 ? (
-                                        <Avatar src={row.img} alt={row.name} />
-                                      ) : null}
-                                      {value}
-                                    </div>
-                                  </TableCell>
-                                );
-                              })}
-                            </TableRow>
-                          ) : null,
-                        )}
-                    </TableBody>
-                  </Table>
-                </div>
-                <TablePagination
-                  rowsPerPageOptions={[10, 25, 100]}
-                  component='div'
-                  count={users.filter(user => user.angel === true).length}
-                  rowsPerPage={rowsPerPage}
-                  page={page}
-                  onChangePage={handleChangePage}
-                  onChangeRowsPerPage={handleChangeRowsPerPage}
-                />
-              </Paper>
-            </Grid>
-          )
-        ) : null}
+        <DashboardTable users={users} />
       </Container>
-      {/* End hero unit */}
     </React.Fragment>
   );
 }
