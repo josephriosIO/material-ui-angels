5ab81156d36544575a2905f24a1407f6e7b4d018
josephriosio <blankworldwide@gmail.com>
2019-12-13 09:48:13 -0800

    remaking all backend functions

--- material-ui-with-login/backend/backend.js
+++ material-ui-with-login/backend/backend.js
@@ -1,5 +1,211 @@
-import { get, update } from '@reshuffle/db';
+require('dotenv').config();
+import { get, update, find, Q } from '@reshuffle/db';
 import { getCurrentUser } from '@reshuffle/server-function';
+
+const { ADMIN_USER_EMAIL } = process.env;
+
+const usersPrefix = 'users__';
+const rolesPrefix = 'roles__';
+
+const Roles = {
+  ADMIN: 'ADMIN',
+  ANGEL: 'ANGEL',
+  STARTUP: 'STARTUP',
+  GUEST: 'GUEST',
+};
+
+const makeDefaultRole = () => {
+  return Object.keys(Roles).reduce((aggr, curr) => {
+    return {
+      ...aggr,
+      [curr]: false,
+    };
+  }, {});
+};
+
+// const uniqueInsert = (arr, ele) => {
+//   const copy = [...arr];
+//   if (!copy.includes(ele)) {
+//     copy.push(ele);
+//   }
+//   return copy;
+// };
+
+async function validateRole(roles) {
+  const user = getCurrentUser(true);
+  const userRoles =
+    (await get(`${rolesPrefix}${user.id}`)) || makeDefaultRole();
+  const validRole = roles.some(role => userRoles[role]);
+  if (!validRole) {
+    throw new Error(`User with id: ${user.id} does not have a valid role for this
+action`);
+  }
+  return user;
+}
+
+async function checkIfUserHasRole(userId, role) {
+  const userRoles = (await get(`${rolesPrefix}${userId}`)) || makeDefaultRole();
+  if (userRoles[role] !== true) {
+    return false;
+  }
+
+  return true;
+}
+
+async function addRoleToUser(id, role) {
+  await update(`${rolesPrefix}${id}`, (roles = makeDefaultRole()) => {
+    return {
+      ...roles,
+      [role]: true,
+    };
+  });
+}
+
+async function removeRoleToUser(id, role) {
+  await update(`${rolesPrefix}${id}`, (roles = makeDefaultRole()) => {
+    return {
+      ...roles,
+      [role]: false,
+    };
+  });
+}
+
+// example of limiting access to a specific role(s)
+/* @expose */
+export async function makeUserAngel(userId) {
+  // await validateRole([Roles.ADMIN])
+
+  if (await checkIfUserHasRole(userId, [Roles.ANGEL])) {
+    await removeRoleToUser(userId, Roles.ANGEL);
+    return;
+  }
+
+  await addRoleToUser(userId, Roles.ANGEL);
+}
+
+/* @expose */
+export async function getAllStartups() {
+  const profile = await validateRole([Roles.ANGEL, Roles.ADMIN]);
+  const rolesQuery = await find(
+    Q.all(Q.key.startsWith(rolesPrefix), Q.value.STARTUP.eq(true)),
+  );
+  const userIds = rolesQuery.map(({ key, value }) => {
+    return key.slice(rolesPrefix.length, key.length);
+  });
+  const usersQuery = await find(
+    Q.filter(
+      Q.any(
+        ...userIds.map(id => {
+          return Q.key.startsWith(`${usersPrefix}${id}`);
+        }),
+      ),
+    ),
+  );
+  return usersQuery.map(({ value }) => value);
+}
+
+/* @expose */
+export async function getAllAngels() {
+  // const profile = await validateRole([Roles.ANGEL, Roles.ADMIN]);
+  const rolesQuery = await find(
+    Q.filter(Q.all(Q.key.startsWith(rolesPrefix), Q.value.ANGEL.eq(true))),
+  );
+
+  const userIds = rolesQuery.map(({ key, value }) => {
+    return key.slice(rolesPrefix.length, key.length);
+  });
+
+  console.log(userIds);
+
+  if (userIds.length < 1) {
+    return [];
+  }
+
+  const usersQuery = await find(
+    Q.filter(
+      Q.any(
+        ...userIds.map(id => {
+          return Q.key.startsWith(`${usersPrefix}${id}`);
+        }),
+      ),
+    ),
+  );
+
+  return usersQuery.map(({ value }) => value);
+}
+
+/* @expose */
+export async function getAllUsersNotAStartup() {
+  const profile = await validateRole([Roles.ANGEL, Roles.ADMIN]);
+  const rolesQuery = await find(
+    Q.all(Q.key.startsWith(rolesPrefix), Q.value.STARTUP.eq(false)),
+  );
+  const userIds = rolesQuery.map(({ key, value }) => {
+    return key.slice(rolesPrefix.length, key.length);
+  });
+  const usersQuery = await find(
+    Q.filter(
+      Q.any(
+        ...userIds.map(id => {
+          return Q.key.startsWith(`${usersPrefix}${id}`);
+        }),
+      ),
+    ),
+  );
+  return usersQuery.map(({ value }) => value);
+}
+/* @expose */
+export async function getRole() {
+  const { id } = getCurrentUser(true);
+  const usersRoles = await update(
+    `${rolesPrefix}${id}`,
+    (roles = makeDefaultRole()) => {
+      return roles;
+    },
+  );
+
+  return usersRoles;
+}
+/* @expose */
+export async function createOrGetUser() {
+  const profile = getCurrentUser(true);
+
+  const addUser = await update(`${usersPrefix}${profile.id}`, user => {
+    console.log(profile.emails[0]);
+    if (profile.emails[0].value === ADMIN_USER_EMAIL) {
+      addRoleToUser(profile.id, Roles.ADMIN);
+    }
+
+    if (user) {
+      return user;
+    }
+    return {
+      id: profile.id,
+      name: profile.displayName,
+      img: profile.picture,
+      email: profile.emails,
+      phoneNumber: null,
+      bio: '',
+      location: '',
+    };
+  });
+
+  return addUser;
+}
+/* expose */
+export async function updateUser(fields) {
+  const { id } = getCurrentUser(true);
+  return update(`${usersPrefix}${id}`, userToUpdate => {
+    if (!userToUpdate) {
+      throw new Error('User does not exist');
+    }
+    const copy = { ...userToUpdate };
+
+    return copy;
+  });
+}
+
+//OLD
 
 /* @expose */
 export async function setStartupsToBackend(profile) {
@@ -74,7 +280,6 @@ export async function getStartups() {
 
     return startups;
   } catch (err) {
-    console.log(err);
     return console.error(err);
   }
 }
@@ -82,6 +287,7 @@ export async function getStartups() {
 /* @expose */
 export async function updateStatus(userId, admin, angel) {
   const { id } = getCurrentUser(true);
+
   return update('angelss', (users = []) => {
     let allUsers = JSON.parse(JSON.stringify(users));
     allUsers.map(user => {
