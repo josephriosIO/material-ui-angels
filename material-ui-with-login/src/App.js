38fce657c5826bdb6b9b17c3d34f963f49c89132
josephriosio <blankworldwide@gmail.com>
2019-12-13 15:23:08 -0800

    added invite path

--- material-ui-with-login/src/App.js
+++ material-ui-with-login/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Routes from './components/Angels/Routes';
 import LandingPage from './components/LandingPage/LandingPage';
 import StartupRoutes from './components/Startups/Routes';
+import AngelInvite from './components/Angels/AngelInvite';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route exact path='/' component={LandingPage} />
         <Route path='/startups' component={StartupRoutes} />
         <Route path='/angels' component={Routes} />
+        <Route exact path={`/invite/:id`} component={AngelInvite} />
       </Switch>
     </>
   );
